113d17120d682c67a1cfddf08db8ea73f5617998
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2025-09-04 10:11:19 +1000

    Update Homebrew formula to v0.1.2 with onefile binaries
    - Update version from 0.1.0 to 0.1.2
    - Update SHA256 hashes for new onefile binaries
    - ARM64: 557f6b4383fc55548af30de7d6378c26d8d9cf00c05f8be4e8bd28085ea3f836
    - x86_64: b1c38c5dcdd8902bbaab2afa462195bdd2a7b47018972b244d96fcd8769e17f1
    - This should fix the PyInstaller runtime error with onefile build

--- Formula/mycli-app.rb
+++ Formula/mycli-app.rb
@@ -2,17 +2,17 @@
 class MycliApp < Formula
   desc "Simple Azure-like CLI tool by Naga"
   homepage "https://github.com/naga-nandyala/mycli-app"
-  version "0.1.0"
+  version "0.1.2"
   license "MIT"
 
   base_url = "https://github.com/naga-nandyala/mycli-app/releases/download/v#{version}"
   
   if Hardware::CPU.arm?
     url "#{base_url}/mycli-#{version}-macos-arm64.tar.gz"
-    sha256 "0cb05d9e0c95a284542753f02dea0887dce480803022b6e45695029335350db8"
+    sha256 "557f6b4383fc55548af30de7d6378c26d8d9cf00c05f8be4e8bd28085ea3f836"
   else
     url "#{base_url}/mycli-#{version}-macos-x86_64.tar.gz"
-    sha256 "8d74eec878a5f241d7f1f3f6585c68d25ddda784ebe19a97b9607c2ab6d0b64c"
+    sha256 "b1c38c5dcdd8902bbaab2afa462195bdd2a7b47018972b244d96fcd8769e17f1"
   end
 
   def install
